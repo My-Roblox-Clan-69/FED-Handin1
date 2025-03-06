881b810e3267080d347e6c61fb1f20b7f06af21f
SimonEifer <202306749@post.au.dk>
2025-03-06 13:18:17 +0100

    adds design features

--- CarWorkshopApp/MauiProgram.cs
+++ CarWorkshopApp/MauiProgram.cs
@@ -3,7 +3,9 @@ using CommunityToolkit.Maui;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using CarWorkshopApp.Data;
-
+using CarWorkshopApp.ViewModels;
+using CarWorkshopApp.Services;
+using CarWorkshopApp.Views;
 
 namespace CarWorkshopApp;
 public static class MauiProgram
@@ -11,18 +13,37 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
-        {
-            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-        }).UseMauiCommunityToolkit();
+        
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .UseMauiCommunityToolkit();
+
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
 
-string dbPath = Path.Combine(FileSystem.AppDataDirectory, "carworkshop.db");
-builder.Services.AddDbContext<CarWorkshopDbContext>(options =>
-    options.UseSqlite($"Data Source={dbPath}"));
+        // Database Path
+        string dbPath = Path.Combine(FileSystem.AppDataDirectory, "carworkshop.db");
+        builder.Services.AddDbContext<CarWorkshopDbContext>(options =>
+            options.UseSqlite($"Data Source={dbPath}"));
+
+        // Register Services
+        builder.Services.AddSingleton<BookingService>();
+
+        // Register ViewModels
+        builder.Services.AddTransient<BookingViewModel>();  // ViewModel for BookingPage
+		builder.Services.AddTransient<OverviewViewModel>();
+
+        // Register Views
+        builder.Services.AddTransient<HomePage>();
+        builder.Services.AddTransient<BookingPage>();
+		builder.Services.AddTransient<OverviewPage>();
+
 
         return builder.Build();
     }
